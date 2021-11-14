090cde127219d2a531dcdae8f9958e9163b73dab
Cliff Choiniere <crcnum4@gmail.com>
2021-11-13 20:17:11 -0500

    cors

--- src/main/java/com/example/dataDemo/devconnect/developers/DeveloperController.java
+++ src/main/java/com/example/dataDemo/devconnect/developers/DeveloperController.java
@@ -6,6 +6,7 @@ import javax.websocket.server.PathParam;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/developers")
 public class DeveloperController {
